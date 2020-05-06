7b11408f539b7552e2e87b7d07ba9c0edcd8b5bc
Ethan Degenhardt <ethandeg1996@gmail.com>
2020-05-06 17:44:38 -0500

    turning in project

---
+++ app.js
@@ -0,0 +1,13 @@
+function findUserByUsername(usersArray, username) {
+	return usersArray.find(function(user) {
+		return user.username === username;
+	});
+}
+
+function removeUser(arr, username) {
+	let foundIndex = arr.findIndex(function(user) {
+		return user.username === username;
+	});
+	if (foundIndex === -1) return;
+	return arr.splice(foundIndex, 1)[0];
+}
